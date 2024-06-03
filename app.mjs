a581141f52e827f15bada82d08d4cbb2e75efa3e
Abdul Karim <abdulkarimmandvia76@gmail.com>
2024-06-04 03:05:42 +0500

    Q6 done

--- app.mjs
+++ app.mjs
@@ -14,3 +14,30 @@ console.log(`Current Month : ${monthNames[currentMonth]}`);
 let daysNames = ["Mon" , "Tues" , "Wed" ,"Thurs" ,"Fri" , "Sat" , "Sun"];
 let currentDay = new Date().getDay()-1;
 console.log(`Today is ${daysNames[currentDay]}`);
+
+//Q:4
+let daysnames = ["Mon" , "Tues" , "Wed" ,"Thurs" ,"Fri" , "Sat" , "Sun"]
+let today = new Date().getDay()-1;
+let todayDayName = daysnames[today]
+console.log(todayDayName)
+if(todayDayName === "Sat" || todayDayName == "Sun"){
+    console.log(`Its Funday`)
+}else{
+    console.log(`Week day`)
+}
+
+//Q:5
+let daysInMonths = new Date().getMonth()-1;
+    console.log(`${daysInMonths}`)
+    if(daysInMonths<=15){
+        alert(`First Fifteen days of the month.`)
+    }else{
+        alert(`Last days of the month.`)
+    }
+
+//Q:6
+let current = new Date();
+console.log(`Current Date: ${current}`)
+
+let elapsedMilliSecond = current.getTime();
+console.log(`${current} & Elapsed Millisecond :${elapsedMilliSecond}`);
